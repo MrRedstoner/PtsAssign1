2ba74593c9fbb2e024b31b2c3e7fddb8d9e1d413
Patrik Grman <pato.grman@gmail.com>
2020-03-07 19:11:28 +0100

    Test refactoring

--- relation_class_test.py
+++ relation_class_test.py
@@ -8,23 +8,23 @@ class MyTestCase(unittest.TestCase):
         res = get_relation_class({1, 2, 3})
 
         obj0 = res()
-        self.assertEqual(False, (1, 2) in obj0)
+        self.assertNotIn((1, 2), obj0)
 
         obj1 = obj0 + (1, 2)
-        self.assertEqual(True, (1, 2) in obj1)
+        self.assertIn((1, 2), obj1)
         # check immutability
-        self.assertEqual(False, (1, 2) in obj0)
+        self.assertNotIn((1, 2), obj0)
 
         # double add an element
         obj2 = (obj1 + (1, 3)) + (1, 2)
-        self.assertEqual(True, (1, 2) in obj2)
-        self.assertEqual(True, (1, 3) in obj2)
+        self.assertIn((1, 2), obj2)
+        self.assertIn((1, 3), obj2)
 
         obj3 = obj2 - (1, 2)
-        self.assertEqual(False, (1, 2) in obj3)
-        self.assertEqual(True, (1, 3) in obj3)
+        self.assertNotIn((1, 2), obj3)
+        self.assertIn((1, 3), obj3)
         # check immutability
-        self.assertEqual(True, (1, 2) in obj2)
+        self.assertIn((1, 2), obj2)
 
     def test_exceptions(self):
         res = get_relation_class({1, 2, 3})
